b06cc889f16abe8a884584d065e84f424a9beeed
Nathan Geschwind <ngeschwi@z3r7p1.42lyon.fr>
2021-11-21 17:25:13 +0100

    fin ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	size_t	j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			j = 0;
+			while (j < std::strlen(argv[i]))
+			{
+				argv[i][j] = std::toupper(argv[i][j]);
+				std::cout << argv[i][j];
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
